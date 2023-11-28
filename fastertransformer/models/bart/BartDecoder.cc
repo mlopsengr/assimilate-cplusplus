83edfe6f03ea38dcd2e8374192e47fbd3dddb900
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-28 09:02:57 +0000

    Adds new TensorParallerlDecoderSelfAttentionLayer

--- fastertransformer/models/bart/BartDecoder.cc
+++ fastertransformer/models/bart/BartDecoder.cc
@@ -1,3 +1,24 @@
 #include "src/fastertransformer/models/bart/BartDecoder.h"
 
-namespace
+namespace fastertransformer {
+
+template<typename T>
+void BartDecoder<T>::initialize()
+{
+  self_attention_layer_ = 
+    new TensorParallelDecoderSelfAttentionLayer<T>(max_batch_size_,
+                                                    head_num_,
+                                                    d_model_,
+                                                    q_scaling_,
+                                                    tensor_para_,
+                                                    stream_,
+                                                    cublas_wrapper_,
+                                                    allocator_,
+                                                    true,
+                                                    is_free_buffer_after_forward_,
+                                                    false,
+                                                    0,
+                                                    custom_all_reduce_comm_,
+                                                    enable_custom_all_reduce_);
+}
+}
