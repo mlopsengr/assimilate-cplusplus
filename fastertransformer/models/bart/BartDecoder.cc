40859270a9ae4e167e0c7b180ad7eb4d96cfc71b
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-12-06 23:35:14 +0000

    Adds first value

--- fastertransformer/models/bart/BartDecoder.cc
+++ fastertransformer/models/bart/BartDecoder.cc
@@ -60,7 +60,8 @@ void BartDecoder<T>::initialize()
   }
   else if (activation_type_ == ActivationType::RElu || activation_type_ == ActivationType::ReGLU){
     ffn_layer_ = new TensorParallelReluFfnLayer<T>(max_batch_size_,
-        ) 
+                                                   1,
+                                                   ) 
   }
 
 }
